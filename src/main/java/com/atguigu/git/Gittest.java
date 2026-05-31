016b557c24e45a76fbf5eef2b5667ba1d2408c67
gengshipeng342 <pb6w6mdn4z@privaterelay.appleid.com>
2026-05-31 10:39:45 +0800

    pull.java

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -9,5 +9,6 @@ public class Gittest {
         System.out.println("hello ");
         System.out.println("hello git45678");
         System.out.println("master");
+        System.out.println("pull");
     }
 }
